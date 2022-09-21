1408f4468ffafbc8b6166ae2abddc9a50ac52ed5
ruk91 <sajithkasn@gmail.com>
2022-09-21 11:30:04 +0530

    Add first typescript app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { country, Person } from './components/Person'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person
+        name='Sajith'
+        email='sajith@gmail.com'
+        age={31}
+        isMarried={true}
+        friends={['jessica', 'jane', 'joe']}
+        country={country.Brazil}
+      />
     </div>
   );
 }
